8c51ffa3639b65ff3665ce8f1b04be152ae6b729
Mahika Jadhav <103935469+MahikaJadhav@users.noreply.github.com>
2022-11-18 21:09:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,33 +2,24 @@ from logging import debug
 from flask import Flask, render_template, request 
 import utils  
 from utils import preprocessdata 
+from constants import features
 
 app = Flask(__name__) 
 
 @app.route('/') 
 def home(): 
-    return render_template('index.html') 
-@app.route('/predict/', methods=['GET', 'POST'])
+    return render_template('index.html', features=features) 
 
-def predict():  
-    if request.method == 'POST': 
-        Gender = request.form.get('Gender')
-        Married = request.form.get('Married')
-        Education = request.form.get('Education')
-        Self_Employed = request.form.get('Self_Employed')  
-        ApplicantIncome = request.form.get('ApplicantIncome')  
-        CoapplicantIncome = request.form.get('CoapplicantIncome') 
-        LoanAmount = request.form.get('LoanAmount')   
-        Loan_Amount_Term = request.form.get('Loan_Amount_Term')   
-        Credit_History = request.form.get('Credit_History')   
-        Property_Area = request.form.get('Property_Area')  
+@app.route('/predict/', methods=['POST'])
+def predict(): 
+        
+    features = request.form.to_dict()
+    for k, v in features.items():
+        features[k] = float(v)
 
-    prediction = utils.preprocessdata(Gender, Married, Education, Self_Employed, ApplicantIncome,
-       CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History,
-       Property_Area)
+    prediction = utils.preprocessdata(features)
 
     return render_template('predict.html', prediction=prediction) 
 
 if __name__ == '__main__': 
     app.run(debug=True) 
-
