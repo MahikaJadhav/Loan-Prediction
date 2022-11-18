51a3851319534db14b2d213785cb2fc10442975c
Mahika Jadhav <103935469+MahikaJadhav@users.noreply.github.com>
2022-11-18 21:16:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,10 @@
 import numpy as np 
+import pandas as pd
 import joblib 
 
+model = joblib.load("./static/Forest.pkl") 
 
-def preprocessdata(Gender, Married, Education, Self_Employed, ApplicantIncome,
-       CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History,
-       Property_Area):
-    test_data = [[Gender, Married, Education, Self_Employed, ApplicantIncome,
-       CoapplicantIncome, LoanAmount, Loan_Amount_Term, Credit_History,
-       Property_Area] ]  
-    trained_model = joblib.load("forest.pkl") 
-    prediction = trained_model.predict(test_data) 
-
-    return prediction 
+def preprocessdata(features):
+   features = pd.DataFrame(features, index=[0])   
+   prediction = model.predict(features)
+   return prediction 
