3da1ffecd2ebf2203cdd852c94b9f6213c450db3
Mahika Jadhav <103935469+MahikaJadhav@users.noreply.github.com>
2022-11-18 21:20:29 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,46 @@
+features = [
+    { 
+        'id': 'Gender',
+        'name': 'Gender',
+    },
+    { 
+        'id': 'Married',
+        'name': 'Married',
+    },
+    { 
+        'id': 'Dependents',
+        'name': 'Dependents',
+    },
+    { 
+        'id': 'Education',
+        'name': 'Education',
+    },
+    { 
+        'id': 'Self_Employed',
+        'name': 'Self Employed',
+    },
+    { 
+        'id': 'ApplicantIncome',
+        'name': 'Applicant Income',
+    },
+    { 
+        'id': 'CoapplicantIncome',
+        'name': 'Coapplicant Income',
+    },
+    { 
+        'id': 'LoanAmount',
+        'name': 'Loan Amount',
+    },
+    { 
+        'id': 'Loan_Amount_Term',
+        'name': 'Loan Amount Term',
+    },
+    { 
+        'id': 'Credit_History',
+        'name': 'Credit History',
+    },
+    { 
+        'id': 'Property_Area',
+        'name': 'Property Area',
+    },
+]
